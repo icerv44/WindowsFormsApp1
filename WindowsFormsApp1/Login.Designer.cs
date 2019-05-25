0f8875fd289c54ecbee5143f5fc92f4aef2ffe65
SoLoRi <SoLoRi@DESKTOP-545RIUK>
2019-05-25 16:06:14 +0700

    commit

--- WindowsFormsApp1/Login.Designer.cs
+++ WindowsFormsApp1/Login.Designer.cs
@@ -30,34 +30,66 @@
         {
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.textBox_User = new System.Windows.Forms.TextBox();
+            this.textBox_Pass = new System.Windows.Forms.TextBox();
+            this.button_Login = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(163, 132);
+            this.label1.Location = new System.Drawing.Point(122, 107);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(87, 17);
+            this.label1.Size = new System.Drawing.Size(66, 13);
             this.label1.TabIndex = 0;
             this.label1.Text = "User Name :";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(165, 177);
+            this.label2.Location = new System.Drawing.Point(124, 144);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(77, 17);
+            this.label2.Size = new System.Drawing.Size(59, 13);
             this.label2.TabIndex = 1;
             this.label2.Text = "Password :";
             // 
+            // textBox_User
+            // 
+            this.textBox_User.Location = new System.Drawing.Point(204, 104);
+            this.textBox_User.Name = "textBox_User";
+            this.textBox_User.Size = new System.Drawing.Size(178, 20);
+            this.textBox_User.TabIndex = 2;
+            // 
+            // textBox_Pass
+            // 
+            this.textBox_Pass.Location = new System.Drawing.Point(204, 144);
+            this.textBox_Pass.Name = "textBox_Pass";
+            this.textBox_Pass.Size = new System.Drawing.Size(178, 20);
+            this.textBox_Pass.TabIndex = 3;
+            // 
+            // button_Login
+            // 
+            this.button_Login.Location = new System.Drawing.Point(204, 197);
+            this.button_Login.Name = "button_Login";
+            this.button_Login.Size = new System.Drawing.Size(80, 27);
+            this.button_Login.TabIndex = 4;
+            this.button_Login.Text = "LogIn";
+            this.button_Login.UseVisualStyleBackColor = true;
+            // 
             // Login
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(727, 407);
+            this.ClientSize = new System.Drawing.Size(545, 331);
+            this.Controls.Add(this.button_Login);
+            this.Controls.Add(this.textBox_Pass);
+            this.Controls.Add(this.textBox_User);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Login";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "LogIn";
@@ -70,5 +102,8 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox_User;
+        private System.Windows.Forms.TextBox textBox_Pass;
+        private System.Windows.Forms.Button button_Login;
     }
 }
